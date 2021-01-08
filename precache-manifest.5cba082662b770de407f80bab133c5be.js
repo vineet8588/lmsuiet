d8da346543517c1c5d140548a5320feb70e5acee
= <=>
2021-01-09 01:42:19 +0530

    Updates

---
+++ precache-manifest.5cba082662b770de407f80bab133c5be.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4e414c6d82e8aa69ecad4802546c89",
+    "url": "/lmsuiet/index.html"
+  },
+  {
+    "revision": "61d7bb23505148b9ca20",
+    "url": "/lmsuiet/static/css/2.1ea2e813.chunk.css"
+  },
+  {
+    "revision": "8afb43d62e58eebd354f",
+    "url": "/lmsuiet/static/css/main.de007b4f.chunk.css"
+  },
+  {
+    "revision": "61d7bb23505148b9ca20",
+    "url": "/lmsuiet/static/js/2.e8a8be86.chunk.js"
+  },
+  {
+    "revision": "a4ad355058fa34d4c6acf09b37800d53",
+    "url": "/lmsuiet/static/js/2.e8a8be86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8afb43d62e58eebd354f",
+    "url": "/lmsuiet/static/js/main.011d36d9.chunk.js"
+  },
+  {
+    "revision": "d50786dfc0ef657f0d4a",
+    "url": "/lmsuiet/static/js/runtime-main.fb63c72e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/lmsuiet/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/lmsuiet/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/lmsuiet/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/lmsuiet/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/lmsuiet/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "87ff362e963dc52c97929e148c87ed09",
+    "url": "/lmsuiet/static/media/undraw_professor.87ff362e.svg"
+  }
+]);
